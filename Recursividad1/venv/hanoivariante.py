13f2361c76049cba8984ae70f85366c3834d3e0a
chaarlie37 <carlos37sm@gmail.com>
2020-03-23 17:05:16 +0100

    hanoi

---
+++ Recursividad1/venv/hanoivariante.py
@@ -0,0 +1,19 @@
+def hanoi(origen, aux, destino, discos):
+    if discos == 1:
+        imprimir(1, origen, aux)
+        imprimir(1, aux, destino)
+    else:
+        hanoi(origen, aux, destino, discos - 1)
+        imprimir(discos, origen, aux)
+        hanoi(destino, aux, origen, discos-1)
+        imprimir(discos, aux, destino)
+        hanoi(origen, aux, destino, discos-1)
+
+
+def imprimir(disco, origen, destino):
+    print("Mueve disco ", disco, " desde torre ", origen, " a torre ", destino)
+
+
+n = int(input())
+
+hanoi(1, 2, 3, n)
