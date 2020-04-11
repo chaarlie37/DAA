866074a5492079967c0f4b13fea90ea482cb8e3f
chaarlie37 <carlos37sm@gmail.com>
2020-04-11 13:07:37 +0200

    prac2

---
+++ Practica2/venv/imprimir_polinomios.py
@@ -0,0 +1,27 @@
+def imprimir_polinomio(lista):
+    if lista == [0]:
+        print(" + 0")
+    elif len(lista) > 0:
+        n = len(lista) - 1
+        if lista[n] < 0:
+            if n == 0:
+                print(" - ", -lista[n], sep="")
+            else:
+                print(" - ", -lista[n], "x^", n, end="", sep="")
+        elif lista[n] > 0:
+            if n == 0:
+                print(" + ", lista[n], sep="")
+            else:
+                print(" + ", lista[n], "x^", n, end="", sep="")
+        imprimir_polinomio(lista[0:n])
+
+
+
+grado = int(input())
+nums = input()
+lista = []
+for i in range(grado + 1):
+    n = int(nums.split(" ")[i])
+    lista.append(n)
+
+imprimir_polinomio(lista)
