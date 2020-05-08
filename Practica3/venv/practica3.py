ae3f91bd405049d0c77d175e5401e5ba416f72f4
chaarlie37 <carlos37sm@gmail.com>
2020-05-08 13:28:57 +0200

    practicas

--- Practica3/venv/practica3.py
+++ Practica3/venv/practica3.py
@@ -6,11 +6,10 @@ def subcolecciones(c, m):
 
 
 def sub_aux(c, subc, m, i, j, colecciones):
-    if i == m and subc[m-1] is not None and coleccion_valida(subc):
+    if i == m and coleccion_valida(subc):
         colecciones.append(subc[0:i])
     if i < m:
-        n = len(c)
-        for k in range(j + 1, n):
+        for k in range(j + 1, len(c)):
             subc[i] = c[k]
             sub_aux(c, subc, m, i + 1, k, colecciones)
     return colecciones
@@ -18,12 +17,11 @@ def sub_aux(c, subc, m, i, j, colecciones):
 
 def coleccion_valida(c):
     subc = c.copy()
-    m = len(subc)
     subc.sort()
     minimo = subc[0]
     valida = True
     i = 1
-    while valida and i < m:
+    while valida and i < len(subc):
         valida = subc[i] % minimo == 0
         i += 1
     return valida
