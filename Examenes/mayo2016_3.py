7c3b30ef86d14dacdd7760303a925903077e5739
chaarlie37 <carlos37sm@gmail.com>
2020-05-31 01:42:07 +0200

    sublistas

--- Examenes/mayo2016_3.py
+++ Examenes/mayo2016_3.py
@@ -3,23 +3,28 @@ def sublistas(lista):
     for i in lista:
         suma += i
     suma = suma / 2
-    subl_rec(suma, lista, [], 0, 0)
+    subconjuntos(suma, lista, [None] * len(lista), [], 0, 0)
 
-def subl_rec(suma, lista, sublista, i, j):
-    if total_lista(sublista) == suma:
-        print("Solucion", sublista)
-    else:
-        if total_lista(sublista) < suma:
-            for k in range(i, len(lista)):
-                # sublista[j] = lista[i]
-                sublista.append(lista[k])
-                print(i, "   ", sublista)
-                if total_lista(sublista) > suma:
-                    print("Poda")
-                    sublista.pop()
-                else:
-                    subl_rec(suma, lista, sublista, k + 1, j + 1)
-        sublista.pop()
+
+def otramitad(original, primeramitad):
+    segundamitad = []
+    for i in original:
+        if i not in primeramitad:
+            segundamitad.append(i)
+    return segundamitad
+
+
+def subconjuntos(suma, lista, sublista, sol_parcial, i, j):
+    for k in range(i, len(lista)):
+        sublista[j] = lista[k]
+        # print(sublista[:j+1])
+        if total_lista(sublista[:j + 1]) > suma:
+            pass
+        elif total_lista(sublista[:j + 1]) == suma and sublista[:j + 1] not in sol_parcial:
+            print("SOLUCION: ", sublista[:j + 1], otramitad(lista, sublista[:j + 1]))
+            sol_parcial.append(otramitad(lista, sublista[:j + 1]))
+        else:
+            subconjuntos(suma, lista, sublista, sol_parcial, k + 1, j + 1)
 
 
 def total_lista(l):
@@ -29,4 +34,4 @@ def total_lista(l):
     return suma
 
 
-sublistas([1,2,3,4,5,9])
+sublistas([1, 2, 3, 4, 5, 9])
