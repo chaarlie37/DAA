deb28ccaeedd8bc2977ea58205cff3702861001f
chaarlie37 <carlos37sm@gmail.com>
2020-05-31 01:45:39 +0200

    sublistas mejor

--- Examenes/mayo2016_3.py
+++ Examenes/mayo2016_3.py
@@ -3,7 +3,7 @@ def sublistas(lista):
     for i in lista:
         suma += i
     suma = suma / 2
-    subconjuntos(suma, lista, [None] * len(lista), [], 0, 0)
+    subconjuntos(suma, lista, [None] * len(lista), 0, 0)
 
 
 def otramitad(original, primeramitad):
@@ -14,17 +14,16 @@ def otramitad(original, primeramitad):
     return segundamitad
 
 
-def subconjuntos(suma, lista, sublista, sol_parcial, i, j):
+def subconjuntos(suma, lista, sublista, i, j):
     for k in range(i, len(lista)):
         sublista[j] = lista[k]
         # print(sublista[:j+1])
         if total_lista(sublista[:j + 1]) > suma:
             pass
-        elif total_lista(sublista[:j + 1]) == suma and sublista[:j + 1] not in sol_parcial:
+        elif total_lista(sublista[:j + 1]) == suma and sublista[:j + 1][0] == lista[0]:
             print("SOLUCION: ", sublista[:j + 1], otramitad(lista, sublista[:j + 1]))
-            sol_parcial.append(otramitad(lista, sublista[:j + 1]))
         else:
-            subconjuntos(suma, lista, sublista, sol_parcial, k + 1, j + 1)
+            subconjuntos(suma, lista, sublista, k + 1, j + 1)
 
 
 def total_lista(l):
