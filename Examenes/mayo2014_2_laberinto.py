69d22ef010c0a8ceda466f3778f931f943223596
chaarlie37 <carlos37sm@gmail.com>
2020-06-04 00:43:05 +0200

    Laberinto funcional

--- Examenes/mayo2014_2_laberinto.py
+++ Examenes/mayo2014_2_laberinto.py
@@ -1,36 +1,22 @@
 import math
 
 
-# 0 derecha | 1 izquierda | 2 abajo | 3 arriba
-def laberinto(n, tablero, pasos_minimos, camino_parcial, p, pasos_acumulados, mejor_sol):
+def laberinto(n, tablero, pasos_minimos, camino_parcial, camino_optimo, p, pasos_acumulados, mejor_sol):
     meta = [n - 1, n - 1]
     if p == meta:
-        if len(camino_parcial) > pasos_acumulados:
-            camino_parcial[pasos_acumulados] = p
-        else:
-            camino_parcial.append(p)
-        if pasos_acumulados <= mejor_sol[0]:
-            if pasos_acumulados <= pasos_minimos[p[0]][p[1]]:
-                pasos_minimos[p[0]][p[1]] = pasos_acumulados
-            print("mejor", mejor_sol)
+        camino_parcial.append(p)
+        if pasos_acumulados < pasos_minimos[p[0]][p[1]]:
+            pasos_minimos[p[0]][p[1]] = pasos_acumulados
+        if pasos_acumulados < mejor_sol[0]:
             mejor_sol[0] = pasos_acumulados
-            print("CAMINO OPTIMO:", camino_parcial)
-            for k in camino_parcial:
-                if tablero[k[0]][k[1]] == "*":
-                    tablero[k[0]][k[1]] = " "
-            for k in camino_parcial[:pasos_acumulados + 1]:
-                tablero[k[0]][k[1]] = "*"
-            imprimir_matriz(tablero)
+            camino_optimo.clear()
+            for i in camino_parcial:
+                camino_optimo.append(i)
     else:
         camino_parcial.append(p)
-        for k in tablero:
-            for j in range(len(k)):
-                if k[j] != "▆":
-                    k[j] = " "
-        for k in camino_parcial:
-            tablero[k[0]][k[1]] = "*"
-        if pasos_acumulados < mejor_sol[0] and pasos_acumulados <= pasos_minimos[p[0]][p[1]]:
+        if pasos_acumulados < mejor_sol[0] and pasos_acumulados < pasos_minimos[p[0]][p[1]]:
             pasos_minimos[p[0]][p[1]] = pasos_acumulados
+            # 0 derecha | 1 izquierda | 2 abajo | 3 arriba
             for i in range(4):
                 nuevo_p = p.copy()
                 if i == 0 and p[1] + 1 < n and tablero[p[0]][p[1] + 1] != "▆":  # derecha
@@ -45,9 +31,17 @@ def laberinto(n, tablero, pasos_minimos, camino_parcial, p, pasos_acumulados, me
                 if nuevo_p != p and (nuevo_p not in camino_parcial) and (
                         distancia_entre_puntos(nuevo_p, meta) < distancia_entre_puntos(p, meta) or pasos_acumulados + 1 <=
                         pasos_minimos[nuevo_p[0]][nuevo_p[1]]):
-                    laberinto(n, tablero, pasos_minimos, camino_parcial[:pasos_acumulados + 1], nuevo_p,
+                    laberinto(n, tablero, pasos_minimos, camino_parcial[:pasos_acumulados + 1], camino_optimo, nuevo_p,
                               pasos_acumulados + 1,
                               mejor_sol)
+
+    if pasos_acumulados == 0 and len(camino_optimo) > 0:
+        print("CAMINO OPTIMO:", camino_optimo)
+        print("Pasos desde la salida hasta la meta:", mejor_sol[0])
+        limpiar_tablero(tablero)
+        for k in camino_optimo:
+            tablero[k[0]][k[1]] = "*"
+        imprimir_matriz(tablero)
 
 
 def distancia_entre_puntos(p1, p2):
@@ -68,9 +62,16 @@ def imprimir_matriz(m):
     print()
 
 
+def limpiar_tablero(tablero):
+    for k in tablero:
+        for j in range(len(k)):
+            if k[j] != "▆":
+                k[j] = " "
+
+
 tablero = [[" ", " ", " ", " ", " ", " ", " ", "▆", "▆", "▆"],
            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-           [" ", " ", " ", "▆", "▆", "▆", "▆", "▆", " ", " "],
+           [" ", " ", " ", "▆", "▆", "▆", "▆", "▆", "▆", " "],
            ["▆", " ", " ", "▆", " ", " ", " ", " ", "▆", " "],
            [" ", "▆", " ", "▆", " ", "▆", " ", " ", " ", "▆"],
            [" ", " ", " ", "▆", " ", " ", "▆", " ", " ", " "],
@@ -79,6 +80,18 @@ tablero = [[" ", " ", " ", " ", " ", " ", " ", "▆", "▆", "▆"],
            [" ", "▆", " ", "▆", "▆", " ", "▆", " ", " ", " "],
            [" ", " ", " ", " ", " ", " ", "▆", "▆", "▆", " "]]
 
+tablero2 = [[" ", " ", " ", " ", " ", " ", " ", "▆", "▆", "▆"],
+           [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+           [" ", " ", " ", "▆", "▆", "▆", "▆", "▆", " ", " "],
+           [" ", " ", " ", "▆", "▆", "▆", " ", " ", " ", " "],
+           [" ", " ", " ", "▆", "▆", "▆", " ", " ", " ", " "],
+           [" ", " ", " ", "▆", " ", " ", " ", " ", " ", " "],
+           [" ", " ", " ", "▆", "▆", "▆", "▆", " ", " ", " "],
+           [" ", " ", " ", "▆", " ", " ", "▆", " ", " ", " "],
+           [" ", " ", " ", "▆", " ", " ", "▆", " ", " ", " "],
+           [" ", " ", " ", " ", " ", " ", "▆", "▆", "▆", " "]]
+
+# para otras matrices de otros tamaños n*n, sustituir 10 por n
 m = []
 for i in range(10):
     aux = []
@@ -86,5 +99,4 @@ for i in range(10):
         aux.append(100)
     m.append(aux)
 
-laberinto(10, tablero, m, [], [0, 0], 0, [60])
-imprimir_matriz(m)
+laberinto(10, tablero, m, [], [], [0, 0], 0, [10*10])
