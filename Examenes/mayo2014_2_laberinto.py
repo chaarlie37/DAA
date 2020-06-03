fa7e77cab63fc5e7ca1f77de9613f67514421078
chaarlie37 <carlos37sm@gmail.com>
2020-06-03 14:34:02 +0200

    Laberinto

--- Examenes/mayo2014_2_laberinto.py
+++ Examenes/mayo2014_2_laberinto.py
@@ -1,43 +1,37 @@
 import math
 
+
 # 0 derecha | 1 izquierda | 2 abajo | 3 arriba
-def laberinto(n, tablero, visitados, camino_parcial, camino_final, p, acumulados):
-    print("p:", p)
-    print("parcial", camino_parcial)
+def laberinto(n, tablero, visitados, camino_parcial, p, acumulados):
+    meta = [n - 1, n - 1]
     visitados.append(p)
-    if p[0] == n - 1 and p[1] == n - 1:
-        camino_parcial.append(p)
-        print("SOLUCION:", camino_parcial)
-        camino_final.append(1)
-        for k in camino_parcial:
+    if p == meta:
+        if len(camino_parcial) > acumulados:
+            camino_parcial[acumulados] = p
+        else:
+            camino_parcial.append(p)
+        print("CAMINO OPTIMO:", camino_parcial)
+        for k in camino_parcial[:acumulados + 1]:
             tablero[k[0]][k[1]] = "*"
-        return None
     else:
-        if len(camino_final) > 0:
-            return None
-        for i in range(3, -1, -1):
-            p_objetivo = [n-1, n-1]
-            nuevo_p = p.copy()
-            if i == 0 and p[1] + 1 < n and tablero[p[0]][p[1] + 1] != "▆":
-                nuevo_p[1] += 1
-                if nuevo_p not in visitados and distancia_entre_puntos(nuevo_p, p_objetivo) < distancia_entre_puntos(p, p_objetivo):
-                    camino_parcial.append(p)
-                    laberinto(n, tablero, visitados, camino_parcial, camino_final, nuevo_p, acumulados + 1)
-            elif i == 1 and p[1] - 1 > 0 and tablero[p[0]][p[1] - 1] != "▆":
-                nuevo_p[1] -= 1
-                if nuevo_p not in visitados and distancia_entre_puntos(nuevo_p, p_objetivo) < distancia_entre_puntos(p, p_objetivo):
-                    camino_parcial.append(p)
-                    laberinto(n, tablero, visitados, camino_parcial, camino_final, nuevo_p, acumulados + 1)
-            elif i == 2 and p[0] + 1 < n and tablero[p[0] + 1][p[1]] != "▆":
-                nuevo_p[0] += 1
-                if nuevo_p not in visitados and distancia_entre_puntos(nuevo_p, p_objetivo) < distancia_entre_puntos(p, p_objetivo):
-                    camino_parcial.append(p)
-                    laberinto(n, tablero, visitados, camino_parcial, camino_final, nuevo_p, acumulados + 1)
-            elif i == 3 and p[0] - 1 > 0 and tablero[p[0] - 1][p[1]] != "▆":
-                nuevo_p[0] -= 1
-                if nuevo_p not in visitados and distancia_entre_puntos(nuevo_p, p_objetivo) < distancia_entre_puntos(p, p_objetivo):
-                    camino_parcial.append(p)
-                    laberinto(n, tablero, visitados, camino_parcial, camino_final, nuevo_p, acumulados + 1)
+        if meta not in camino_parcial:
+            for i in range(3):
+                nuevo_p = p.copy()
+                if i == 0 and p[1] + 1 < n and tablero[p[0]][p[1] + 1] != "▆":      # derecha
+                    nuevo_p[1] += 1
+                elif i == 1 and p[1] - 1 > 0 and tablero[p[0]][p[1] - 1] != "▆":    # izquierda
+                    nuevo_p[1] -= 1
+                elif i == 2 and p[0] + 1 < n and tablero[p[0] + 1][p[1]] != "▆":    # abajo
+                    nuevo_p[0] += 1
+                elif i == 3 and p[0] - 1 > 0 and tablero[p[0] - 1][p[1]] != "▆":    # arriba
+                    nuevo_p[0] -= 1
+
+                if nuevo_p not in visitados and distancia_entre_puntos(nuevo_p, meta) < distancia_entre_puntos(p, meta):
+                    if len(camino_parcial) > acumulados:
+                        camino_parcial[acumulados] = p
+                    else:
+                        camino_parcial.append(p)
+                    laberinto(n, tablero, visitados, camino_parcial, nuevo_p, acumulados + 1)
 
 
 def distancia_entre_puntos(p1, p2):
@@ -45,8 +39,17 @@ def distancia_entre_puntos(p1, p2):
 
 
 def imprimir_matriz(m):
+    for i in range(len(m) + 1):
+        print("___", end="")
+    print()
     for i in m:
-        print(i)
+        print("|", end=" ")
+        for j in i:
+            print(j, "", end=" ")
+        print("|")
+    for i in range(len(m) + 1):
+        print("___", end="")
+    print()
 
 
 tablero = [[" ", " ", " ", " ", " ", " ", " ", "▆", "▆", "▆"],
@@ -60,7 +63,5 @@ tablero = [[" ", " ", " ", " ", " ", " ", " ", "▆", "▆", "▆"],
            [" ", " ", " ", "▆", " ", " ", "▆", " ", " ", " "],
            [" ", " ", " ", " ", " ", " ", "▆", "▆", "▆", " "]]
 
-
-
-laberinto(10, tablero, [], [], [], [0,0], 0)
+laberinto(10, tablero, [], [], [0, 0], 0)
 imprimir_matriz(tablero)
