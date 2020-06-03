c6183e6f359cb96c3cb2bdcac4519b9e73427a25
chaarlie37 <carlos37sm@gmail.com>
2020-06-03 17:45:52 +0200

    laberinto sin terminar

--- Examenes/mayo2014_2_laberinto.py
+++ Examenes/mayo2014_2_laberinto.py
@@ -3,7 +3,6 @@ import math
 
 # 0 derecha | 1 izquierda | 2 abajo | 3 arriba
 def laberinto(n, tablero, visitados, camino_parcial, p, acumulados):
-    print(camino_parcial)
     meta = [n - 1, n - 1]
     visitados.append(p)
     if p == meta:
@@ -27,18 +26,33 @@ def laberinto(n, tablero, visitados, camino_parcial, p, acumulados):
                 elif i == 3 and p[0] - 1 >= 0 and tablero[p[0] - 1][p[1]] != "▆":    # arriba
                     nuevo_p[0] -= 1
 
-                if nuevo_p != p and nuevo_p not in visitados and distancia_entre_puntos(nuevo_p, meta) <= distancia_entre_puntos(p, meta):
+                if p == [2,2]:
+                    print("a")
+
+                if nuevo_p != p and (nuevo_p not in visitados or acumulados < len(visitados)) and distancia_entre_puntos(nuevo_p, meta) < distancia_entre_puntos(p, meta):
                     if len(camino_parcial) > acumulados:
                         camino_parcial[acumulados] = p
                     else:
                         camino_parcial.append(p)
                     laberinto(n, tablero, visitados, camino_parcial, nuevo_p, acumulados + 1)
-                elif nuevo_p != p and nuevo_p not in visitados and len():
-                    if len(camino_parcial) > acumulados:
-                        camino_parcial[acumulados] = p
-                    else:
-                        camino_parcial.append(p)
-                    laberinto(n, tablero, visitados, camino_parcial, nuevo_p, acumulados + 1)
+            if len(camino_parcial) == acumulados:
+                for i in range(4):
+                    nuevo_p = p.copy()
+                    if i == 0 and p[1] + 1 < n and tablero[p[0]][p[1] + 1] != "▆":  # derecha
+                        nuevo_p[1] += 1
+                    elif i == 1 and p[1] - 1 >= 0 and tablero[p[0]][p[1] - 1] != "▆":  # izquierda
+                        nuevo_p[1] -= 1
+                    elif i == 2 and p[0] + 1 < n and tablero[p[0] + 1][p[1]] != "▆":  # abajo
+                        nuevo_p[0] += 1
+                    elif i == 3 and p[0] - 1 >= 0 and tablero[p[0] - 1][p[1]] != "▆":  # arriba
+                        nuevo_p[0] -= 1
+
+                    if nuevo_p != p and nuevo_p not in visitados:
+                        if len(camino_parcial) > acumulados:
+                            camino_parcial[acumulados] = p
+                        else:
+                            camino_parcial.append(p)
+                        laberinto(n, tablero, visitados, camino_parcial, nuevo_p, acumulados + 1)
 
 
 def distancia_entre_puntos(p1, p2):
